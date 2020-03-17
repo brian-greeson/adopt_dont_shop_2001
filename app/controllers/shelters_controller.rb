a9fb33a73aec322261dd739bb752c561eec0c023
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-17 07:12:17 -0600

    Rails genereate controller

---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,16 @@
+class SheltersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def read
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'shelters/index'
+
+  get 'shelters/create'
+
+  get 'shelters/read'
+
+  get 'shelters/update'
+
+  get 'shelters/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 end
