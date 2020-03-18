ac23c1541ef395b93af92e3f31e14a63cc36cb2c
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-17 20:55:27 -0600

    Update test to look for link

--- spec/features/shelters/user_can_create_new_shelters_spec.rb
+++ spec/features/shelters/user_can_create_new_shelters_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "when user", type: :feature do
   it "visit index page can click new shelter button" do
     visit '/shelters'
 
-    expect(page).to have_button('Create Shelter')
+    expect(page).to have_link('Create Shelter')
   end
 
 end
