1e2b7f6efd509d8a61426e1f68b91b3daf7cce6a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-17 21:17:38 -0600

    add current path test

--- spec/features/shelters/user_can_create_new_shelters_spec.rb
+++ spec/features/shelters/user_can_create_new_shelters_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "when user", type: :feature do
     expect(page).to have_link('Create Shelter')
   end
 
+  it "clicks on create shelter link is shown create shelter form" do
+    visit '/shelters'
+    click_link 'Create Shelter'
+
+    expect(page).to have_current_path('/shelters/new')
+  end
+
 end
