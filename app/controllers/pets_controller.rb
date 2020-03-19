8d5b6466a2e36de0227a8a2426a9547e34307866
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-19 07:57:11 -0600

    Add copy of shelter crud files for pets

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,45 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def create
+    Pet.create(
+      name:     params[:pet][:name],
+      image:  params[:pet][:image],
+      age:     params[:pet][:age],
+      sex:    params[:pet][:sex],
+      shelter_id:      params[:pet][:shelter_id]
+    )
+    redirect_to '/pets'
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    Pet.find(params[:id]).update(
+      name:     params[:pet][:name],
+      image:  params[:pet][:image],
+      age:     params[:pet][:age],
+      sex:    params[:pet][:sex],
+      shelter_id:      params[:pet][:shelter_id]
+    )
+
+    redirect_to '/pets'
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def destroy
+    Pet.destroy(params[:id])
+
+    redirect_to '/pets'
+  end
+
+  def new
+  end
+end
