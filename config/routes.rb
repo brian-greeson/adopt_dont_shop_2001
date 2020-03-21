b02a405ba5aac06b87a233d0426ac030cf3889e0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-21 16:08:31 -0600

    Change welcome route to shelters index

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,5 @@ Rails.application.routes.draw do
 
   post '/shelters/:shelter_id/pets', to: 'pets#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#index'
+  get '/', to: 'shelters#index'
 end
