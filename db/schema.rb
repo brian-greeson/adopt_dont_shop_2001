08cb1342b65ebcca710d15a0130c3124d4fe2c02
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-21 10:22:14 -0600

    add adoption status to pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319192143) do
+ActiveRecord::Schema.define(version: 20200321161011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200319192143) do
     t.integer "age"
     t.string "sex"
     t.bigint "shelter_id"
+    t.string "adoption_status", default: "available"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
