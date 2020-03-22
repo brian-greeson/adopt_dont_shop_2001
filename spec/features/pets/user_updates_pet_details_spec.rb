31d614d1aa49d1b74656e9930f7fef8c36009463
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-21 23:02:19 -0600

    Update link test with db entries

--- spec/features/pets/user_updates_pet_details_spec.rb
+++ spec/features/pets/user_updates_pet_details_spec.rb
@@ -3,6 +3,21 @@ require "rails_helper"
 RSpec.describe "when user " do
 
   it "visits Pet show page sees a link to update pet" do
+    shelter_1 = Shelter.create(
+                              name: "shelter 1",
+                              address: "111 shelter dr",
+                              city: "Shelterville",
+                              state: "Sheltelvania",
+                              zip: "01234"
+                            )
+    pet_1 = Pet.create(
+                              name: "pet 1",
+                              image: "5558679305.jpg",
+                              age: 9,
+                              sex: "male",
+                              shelter_id: shelter_1.id
+                            )
+
     visit "pets/#{pet_1.id}"
 
     click_on "update_pet"
