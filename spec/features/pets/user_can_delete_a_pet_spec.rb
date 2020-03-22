57adc68ef2ee38b46e3e9545cc1869d38ac0ad5d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-22 11:40:44 -0600

    Add test for delete pet link on shelter pets

--- spec/features/pets/user_can_delete_a_pet_spec.rb
+++ spec/features/pets/user_can_delete_a_pet_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe "when a user" do
     expect(Pet.where(id: pet_1.id)).to eq([])
   end
 
-  it "visits /pets they see a link to delete the pet" do
+  it "visits /pets they see a link to delete each pet" do
     shelter_1 = Shelter.create(
                               name: "shelter 1",
                               address: "111 shelter dr",
@@ -42,6 +42,32 @@ RSpec.describe "when a user" do
                               shelter_id: shelter_1.id
                             )
     visit "/pets"
+
+    within(".pet_#{pet_1.id}_details") do
+      click_on("Delete Pet")
+    end
+
+    expect(current_path).to eq("/pets")
+    expect(page).to_not have_content(pet_1.name)
+    expect(Pet.where(id: pet_1.id)).to eq([])
+  end
+
+  it "visits /shelters/:id/pets they see a link to delete each pet" do
+    shelter_1 = Shelter.create(
+                              name: "shelter 1",
+                              address: "111 shelter dr",
+                              city: "Shelterville",
+                              state: "Sheltelvania",
+                              zip: "01234"
+                            )
+    pet_1 = Pet.create(
+                              name: "pet 1",
+                              image: "5558679305.jpg",
+                              age: 9,
+                              sex: "male",
+                              shelter_id: shelter_1.id
+                            )
+    visit "/shelters/#{shelter_1.id}/pets"
 
     within(".pet_#{pet_1.id}_details") do
       click_on("Delete Pet")
